2b8d014a099f509d40afc875e8535076d0a2304c
jorgelinadev <jorgelinalberti@gmail.com>
2022-07-08 10:28:57 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,13 @@
+//alert ('Mi archivo está bien linkeado');
+
+//Múltiplos
+
+//Crear un programa que pida al usuario un número, luego otro y mostrar en un mensaje si el primer valor es
+//múltiplo del segundo. INVESTIGAR EL OPERADOR MÓDULO O DE RESTO % PARA HACERLO
+
+const primerNumero = prompt ('Ingrese un número');
+const segundoNumero = prompt ('Ingrese otro número');
+
+alert(`¿El valor ${primerNumero} es múltiplo de ${segundoNumero}? : ${ primerNumero % segundoNumero === 0 }`);
+
+//backsticks `` sirven para interactuar con el código desde un string, se escriben con Alt + 96.
